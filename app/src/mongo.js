bbd31573a6ff4cfc9a094d260ea4b14d8b547ead
shahjalal7311 <mdjalal2012@gmail.com>
2020-10-18 18:25:09 +0600

    first commit

---
+++ app/src/mongo.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+export const uri = "mongodb://localhost:27017/parcelkoi";
+const options = {};
+
+
+const log = (msg) => console.log(msg);
+
+export const connectWithDb = () => {
+    mongoose.connect(uri, options, (err, db) => {
+        if (err) {
+            console.error(err);
+        }
+
+        else log("database connection established");
+    });
+};
